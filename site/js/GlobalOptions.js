c1b1d3f877c2d831d600b9a27904ca45a92aa332
Tobias Reber <tobias.reber@bd.so.ch>
2014-09-23 14:37:11 +0200

    ready

--- site/js/GlobalOptions.js
+++ site/js/GlobalOptions.js
@@ -290,7 +290,7 @@ var OverviewMapMaximized = true; // is the overview map opend or closed by defau
 var overviewLayer = new OpenLayers.Layer.WMTS({
     projection: new OpenLayers.Projection('EPSG:21781'),
     name: "Strassenkarte_farbig",
-    url: "http://srsofaioi12288.ktso.ch/mapcache/wmts",
+    url: "http://www.sogis1.so.ch/mapcache/wmts",
     requestEncoding: 'REST',
     buffer: 0,
     zoomOffset: 15,
