2d7712fd5297046e9a05d81facf8105403945383
Tobias Reber <tobias.reber@bd.so.ch>
2016-08-02 12:04:04 +0200

    wmts on geowebso

--- site/js/GlobalOptions.js
+++ site/js/GlobalOptions.js
@@ -329,7 +329,7 @@ var OverviewMapMaximized = true; // is the overview map opend or closed by defau
 var overviewLayer = new OpenLayers.Layer.WMTS({
     projection: new OpenLayers.Projection('EPSG:21781'),
     name: "Strassenkarte_farbig",
-    url: "http://geoweb-t.rootso.org/mapcache/wmts",
+    url: "http://geoweb.so.ch/mapcache/wmts",
     requestEncoding: 'REST',
     buffer: 0,
     zoomOffset: 15,
